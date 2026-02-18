5a7befcb8c07d805b12c175402b8944274e225e4
colinvarleya456 <colinvarleya456@gmail.com>
2026-02-18 14:04:29 +0000

    crouch cast

--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -2,20 +2,26 @@ extends CharacterBody3D
 
 
 @onready var camera_3d: Camera3D = $Camera3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
+@export_category("Camera")
 @export var camMaxAngle : float = 90
 @export var camMinAngle : float = 0
 @export var camSensHorizontal : float = .2
 @export var camSensVertical : float = .2
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export_category("Player")
+@export var standingHeight : float = 2
+@export var crouchHeight : float = 1
+@export var proneHeight : float = .5
+@export var crouchSpeed : float = .1
+@export var proneSpeed : float = 1
+
+@export var movementSpeed = 5.0
+@export var jumpVelocity = 4.5
 
 func _ready() -> void:
-	pass
-
-
+	assignPlayerInfo()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,21 +29,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpVelocity
 
 	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * movementSpeed
+		velocity.z = direction.z * movementSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, movementSpeed)
+		velocity.z = move_toward(velocity.z, 0, movementSpeed)
 
 	move_and_slide()
 
 func _process(delta: float) -> void:
 	pass
+
+@export var isCrouch : bool = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -49,3 +57,34 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if Input.is_action_just_pressed("x"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if Input.is_action_pressed("control"):
+		if !isCrouch:
+			animation_player.play("crouch")
+			isCrouch = !isCrouch
+		else:
+			if crouch_cast.is_colliding() == false:
+				animation_player.play_backwards("crouch")
+				isCrouch = !isCrouch
+	
+	#elif Input.is_action_just_released("control"):
+	#	
+
+
+
+
+@onready var player_collision: CollisionShape3D = $playerCollision
+
+@onready var crouch_cast: ShapeCast3D = $crouchCast
+
+func assignPlayerInfo():
+	player_collision.shape.height = standingHeight
+	camera_3d.position = Vector3(0,standingHeight * .875,0)
+	
+	animation_player.get_animation("crouch").track_set_key_value(0,0, standingHeight)
+	animation_player.get_animation("crouch").track_set_key_value(0,1, crouchHeight)
+	animation_player.get_animation("crouch").track_set_key_value(1,0, Vector3(0,standingHeight * .875,0))
+	animation_player.get_animation("crouch").track_set_key_value(1,1, standingHeight)
+	
+	animation_player.speed_scale = 1 / crouchSpeed
+	
