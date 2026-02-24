a6202f651716c831737d254b757ce98bffab42f9
Andyreas67 <25459708@studentmail.ul.ie>
2026-02-24 21:50:41 +0000

    Delete Scripts directory (Adding back my changed version)

--- Scripts/bullet_decal.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-@onready var anim: AnimationPlayer = $anim
-
-func _ready() -> void:
-	anim.play("bullet hole fade")
--- Scripts/interactables/interactionTemplate.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody3D
-
-signal interact(player)
-
-func _ready() -> void:
-	connect("interact", interactFunc)
-
-func interactFunc(player):
-	print("interact called : ", player)
