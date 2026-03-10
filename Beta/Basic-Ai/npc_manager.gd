48bfbedcbfd4ce47485eec53046d3e931818a931
Kurash1 <clasheroff7@gmail.com>
2026-03-10 02:56:11 +0000

    Avoidance

--- Beta/Basic-Ai/npc_manager.gd
+++ Beta/Basic-Ai/npc_manager.gd
@@ -4,13 +4,14 @@ extends Node
 
 @onready var npc_scene: PackedScene = preload("res://Beta/Basic-Ai/task_npc.tscn")
 
-@export var marker_entrance: Marker3D
+@export var entrances: Array[Marker3D]
+@export var exits: Array[Marker3D]
 @export var stores: Array[Marker3D]
 
-const npc_amount: int = 64
+const npc_amount: int = 100
 var npc_array: Array[TaskNPC] = []
 
-const SPAWN_TIMER_TICK = 2
+const SPAWN_TIMER_TICK = 0.5
 var spawn_timer = 0
 
 func _process(delta: float) -> void:
@@ -19,8 +20,10 @@ func _process(delta: float) -> void:
 		spawn_timer = 0
 		if (npc_array.size() <= npc_amount):
 			var new_npc: TaskNPC = npc_scene.instantiate()
-			new_npc.position = marker_entrance.position
-			new_npc.tasks = [stores.pick_random(), marker_entrance]
+			var entrance: Marker3D = entrances.pick_random()
+			var exit: Marker3D = exits.pick_random()
+			new_npc.position = entrance.position
+			new_npc.tasks = [stores.pick_random(), exit]
 			new_npc.manager = self
 			add_child(new_npc)
 			npc_array.append(new_npc)
--- Beta/Basic-Ai/task_npc.gd
+++ Beta/Basic-Ai/task_npc.gd
@@ -7,30 +7,29 @@ extends CharacterBody3D
 @export var tasks: Array[Marker3D]
 var current_task = 0
 var manager: NPCManager
+var current_path: Vector3
 
 const SPEED = 5.0
 const THINK_TICK = 1
 
 func _ready() -> void:
-	set_movement_target(tasks[current_task].position)
+	agent.target_position = tasks[current_task].position
 	agent.velocity_computed.connect(Callable(_on_velocity_computed))
-
-func set_movement_target(movement_target: Vector3):
-	agent.target_position = movement_target
 
 func _physics_process(delta: float) -> void:
 	if NavigationServer3D.map_get_iteration_id(agent.get_navigation_map()) == 0:
 		return
-	if agent.is_navigation_finished():
+	if agent.is_navigation_finished() or position.distance_to(agent.target_position) < agent.path_desired_distance:
 		current_task += 1
 		if (current_task >= tasks.size()):
 			manager.npc_array.erase(self)
 			queue_free()
 			return
-		set_movement_target(tasks[current_task].position)
+		agent.target_position = tasks[current_task].position
+		return
 	
-	var next_path_position: Vector3 = agent.get_next_path_position()
-	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * SPEED
+	current_path = agent.get_next_path_position()
+	var new_velocity: Vector3 = global_position.direction_to(current_path) * SPEED
 	if agent.avoidance_enabled:
 		agent.velocity = new_velocity
 	else:
